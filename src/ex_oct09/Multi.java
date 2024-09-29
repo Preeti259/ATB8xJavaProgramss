9b505cbe8cf437b74c09e6d19ebfb3851b574f47
preeti <preeti250993@gmail.com>
2024-09-29 17:28:27 +0530

    all files

---
+++ src/ex_oct09/Multi.java
@@ -0,0 +1,23 @@
+package ex_oct09;
+
+public class Multi {
+    public void dog()
+
+    {
+        System.out.println("i am dog");
+    }
+
+    public void cat()
+
+    {
+        System.out.println("i am cat");
+
+    }
+
+    public void cow()
+
+    {
+        System.out.println("i am cow");
+
+    }
+}
---
+++ src/ex_oct09/SinglelevelInheritance.java
@@ -0,0 +1,14 @@
+package ex_oct09;
+
+public class SinglelevelInheritance extends SingleLevel{
+    public static <SingleLevelInheritance> void main(String[] args)
+    {
+        SingleLevel sli = new SinglelevelInheritance();
+        sli.eat();
+        sli.walk();
+        sli.work();
+        sli.sleep();
+
+    }
+
+}
